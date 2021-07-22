ac3a540a8fb4e8cb7deab7bdc8674573798c601a
Renvolt <djk711@gmail.com>
2021-07-22 16:03:58 -0400

    stoof

--- i3lock/lock.sh
+++ i3lock/lock.sh
@@ -1,44 +1,36 @@
 #!/bin/sh
 
-B='#ffe6ff'  # blank
-C='#ffe6ff'  # clear ish
-D='#ff4dff'  # default
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#ff00ffcc'  # default
 T='#ee00eeee'  # text
-W='#e60000'  # wrong
-V='#ff4dff'  # verifying
+W='#880000bb'  # wrong
+V='#bb00bbbb'  # verifying
 
-/usr/bin/i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
-\
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
-\
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
-\
---verifcolor=$T        \
---wrongcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$W       \
---bshlcolor=$W        \
-\
---screen 1            \
---image ~/.config/i3lock/lockscreen.png              \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%A, %m-%d-%Y" \
-#--keylayout 1         \
-
-# --veriftext="Drinking verification can..."
-# --wrongtext="Nope!"
-# --textsize=20
-# --modsize=10
-# --timefont=comic-sans
-# --datefont=monofur
-# etc
+i3lock \
+	--insidever-color=$C   \
+	--ringver-color=$V     \
+	\
+	--insidewrong-color=$C \
+	--ringwrong-color=$W   \
+	\
+	--inside-color=$B      \
+	--ring-color=$D        \
+	--line-color=$B        \
+	--separator-color=$D   \
+	\
+	--verif-color=$T        \
+	--wrong-color=$T        \
+	--time-color=$T        \
+	--date-color=$T        \
+	--layout-color=$T      \
+	--keyhl-color=$W       \
+	--bshl-color=$W        \
+	\
+	--screen 1            \
+	--blur 8              \
+	--clock               \
+	--indicator           \
+	--time-str="%H:%M:%S"  \
+	--date-str="%A, %m %Y" \
+	--keylayout 1         \
